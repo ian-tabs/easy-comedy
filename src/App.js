f83b6c2af22dca949a9b10b47de40e0ab4c1d69e
Adrian Tabisaura <tabisaura.adrian@gmail.com>
2023-05-28 19:58:14 +0800

    updated app.js added simple searchbar import from antdesign

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+import * as actions from './redux/actions';
 
 function App() {
+
+  const jokeParams = useSelector(state => state.jokeParams)
+  const mainScreen = useSelector(state => state.mainScreen)
+
+
   return (
     <div className="App">
     </div>
---
+++ src/components/searchbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Input } from "antd";
+
+const KeyWordSearchBar = () => {
+
+    return (
+        <Input placeholder="Enter a keyword or leave it blank for a broader range" />
+    )
+
+}
+
+export default KeyWordSearchBar;
